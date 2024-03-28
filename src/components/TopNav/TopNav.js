a64d33a015faa77f332c8572ec0ae8f33059c2eb
Gyani Swarnajeet Singh Amarjeet Singh <Gyani.Singh@coforge.com>
2024-03-28 12:36:04 +0530

    fashion-cart

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,28 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import  './_top-nav.scss'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import "./_top-nav.scss";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 //import {GoogleLogin} from "react-google-login"
 //import {gapi} from 'gapi-script';
-const TopNav = function()
-{
-
-
-  const successHandler=(res)=>{
-
+const TopNav = function () {
+  const successHandler = (res) => {
     console.log(res);
-  }
-  const cartItemCount= useSelector(state=>state.cartReducer.totalItems);
-//console.log("count",cartItemCount);
-    return(
-        <div className='header bg-dark'>
-        <div className='topnavrow row'>
-          <div className='brand my-1'>
-            <h1> Coforge Rewards Shop </h1>
-          </div>
-         {/*
+  };
+  const cartItemCount = useSelector((state) => state.cartReducer.totalItems);
+  //console.log("count",cartItemCount);
+
+
+  return (
+    <div className="header bg-dark" >
+      <div className="topnavrow row">
+        <div className="brand my-1">
+          <h1> Coforge Rewards Shop </h1>
+        </div>
+        {/*
          
           <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
             <div className='dropdown m-0 p-0'>
@@ -42,24 +39,37 @@ const TopNav = function()
               </h5>
           </div>
           */}
-          <div className='cart-wishlist'>
-            <ul className="p-0">
- 
-           
-           <li className="list-icon" > <Link to="/cart"> <FontAwesomeIcon icon="fa fa-shopping-cart" style={{color:"white"}}/>{cartItemCount>0 && <div className='cart-item-count'><p>{cartItemCount}</p></div>}</Link> </li>
-         
-            </ul>
+        <div className="cart-wishlist">
+          <ul className="p-0">
+            <li className="list-icon"> 
+             
+              <Link to="/cart">
+   
 
-          </div>
+         
+          <div className="cart-item-count" >
         
+        <p>{cartItemCount}  </p>
+             <FontAwesomeIcon
+           icon="fa fa-shopping-cart"
+           style={{ color: "white" }}
+         />
+           
+       
+       </div>
+    
+           
+                 
+                
+
+
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-
-    )
-
-
-
-
-}
+    </div>
+  );
+};
 
 export default TopNav;
